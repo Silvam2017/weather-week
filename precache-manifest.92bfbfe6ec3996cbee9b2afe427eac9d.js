9a1d184c0e21c616b23348d456b3fdbc17d6651a
Michael Silva <msilva@aggienetwork.com>
2020-09-10 18:49:40 -0500

    Updates

---
+++ precache-manifest.92bfbfe6ec3996cbee9b2afe427eac9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26890f260ea9d67417c1a6c75fe47247",
+    "url": "/weather-week/index.html"
+  },
+  {
+    "revision": "92eac72e01a6e9b00c72",
+    "url": "/weather-week/static/css/main.f03ef4cb.chunk.css"
+  },
+  {
+    "revision": "52b53f72360058cd1441",
+    "url": "/weather-week/static/js/2.b27a5fad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-week/static/js/2.b27a5fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92eac72e01a6e9b00c72",
+    "url": "/weather-week/static/js/main.b86a8b61.chunk.js"
+  },
+  {
+    "revision": "b1a6fc2427acbf0457b3",
+    "url": "/weather-week/static/js/runtime-main.7a4de167.js"
+  }
+]);
--- src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-const envVariables = process.env
-const {weatherAPIKey} = envVariables
-
-function cityFetch () {
-fetch('api.openweathermap.org/data/2.5/weather?q=' + {city} + '&appid=' + process.env.weatherAPIKey')
-  .then(response => response.json())
-};
